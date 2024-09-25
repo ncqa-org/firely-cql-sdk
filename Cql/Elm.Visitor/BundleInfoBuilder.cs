16232a0a7709bcf771d8dd6d9fb8173af0f04113
Alexander Chisholm <achisholm@ncqa.org>
2024-09-25 10:40:51 -0400

    Update BundleInfoBuilder.cs

--- Cql/Elm.Visitor/BundleInfoBuilder.cs
+++ Cql/Elm.Visitor/BundleInfoBuilder.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hl7.Fhir.Model;
 
 namespace Hl7.Cql.Elm.Visitor
 {
 
     public static class BundleInfoBuilder
     {
-        public static ValueSetMapInfo
+        /// <summary>
+        /// Returns a mapping between 
+        /// </summary>
+        /// <returns></returns>
+        public static Dictionary<FHIRAllTypes, string> GetValueSetInfo()
+        {
+
+        }
+
+        /// <summary>
+        /// Returns a list of resources referenced in the provided statement.
+        /// </summary>
+        /// <returns></returns>
+        public static Dictionary<string, string[]> GetMustSupportResourceInfo()
+        {
+
+        }
     }
 }
